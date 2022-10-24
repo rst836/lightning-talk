5921843bd7953fe26784842536d43c8395b0a5d7
ricky <rickytitus2001@gmail.com>
2022-10-24 02:31:12 -0500

    initial commit

---
+++ demo.py
@@ -0,0 +1,35 @@
+import cv2
+
+cap = cv2.VideoCapture("highway.mp4")
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+while True:
+    # read video frames
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
+    roi = frame[340:720, 500:800]
+
+    # detect object(s)
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+
+        # calculate area and remove small items
+        area = cv2.contourArea(contour)
+        if area > 100:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    # display frame
+    cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    # exit condition (esc key)
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
